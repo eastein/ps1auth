846f433c854ebdf7de2ff30eaffacfa279766282
hef <hef@pbrfrat.com>
2015-05-05 09:38:01 -0500

    more door control features

--- rfid/models.py
+++ rfid/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-#from django.contrib.auth.models import User
 from django.conf import settings
+import logging
+from model_utils.managers import InheritanceManager
 import urllib.request
+
+logger = logging.getLogger(__name__)
 
 
 class Resource(models.Model):
     name = models.CharField(max_length=160)
+    display_name = models.CharField(max_length=160)
 
     def is_allowed(self, tag):
         """
@@ -40,9 +44,16 @@ class WebUnlock(models.Model):
     def __str__(self):
         return "{} Unlock".format(self.resource.name)
 
-    def unlock(self):
-        return urllib.request.urlopen(self.url)
-
+    def unlock(self, user, ip_address):
+        event = ButtonPressLogEvent(
+            resource = self.resource,
+            user = user,
+            ip_address = ip_address,
+        )
+        logger.info(ip_address)
+        event.save()
+        result = urllib.request.urlopen(self.url)
+        return result
 
 class RFIDNumber(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
@@ -52,11 +63,22 @@ class RFIDNumber(models.Model):
         return u'user={}, RFID={}'.format(self.user, self.ASCII_125khz)
 
 class LogEvent(models.Model):
-    models.ForeignKey('Resource')
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    resource = models.ForeignKey('Resource')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_on = models.DateTimeField(auto_now_add=True, blank=False)
+    objects = InheritanceManager()
 
 class RFIDAccessLogEvent(LogEvent):
-    pass
+    rfid_number = models.ForeignKey('RFIDNumber', blank=False)
+    original_key = models.CharField(max_length=12, blank=False)
 
 class ButtonPressLogEvent(LogEvent):
-    pass
+    ip_address = models.GenericIPAddressField(blank=False)
+
+    def __str__(self):
+        return "ButtonPressLogEvent(resource={},user={},created_on={},ip_addres={})".format(
+            self.resource,
+            self.user,
+            self.created_on,
+            self.ip_address,
+        )
--- rfid/views.py
+++ rfid/views.py
@@ -1,9 +1,12 @@
-from .models import Resource, RFIDNumber, WebUnlock, LogEvent
-from .forms import KeyForm
+from datetime import timedelta
 from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Resource, RFIDAccessLogEvent, RFIDNumber, WebUnlock, LogEvent
+from .forms import KeyForm
+
 
 
 def check(request, resource_name, tag_number):
@@ -13,10 +16,16 @@ def check(request, resource_name, tag_number):
     except (RFIDNumber.DoesNotExist, Resource.DoesNotExist):
         return HttpResponse(content="No", status=404, reason="Resource or Tag not Found")
     if resource.is_allowed(tag):
+        log_event = RFIDAccessLogEvent(
+            resource = resource,
+            user = tag.user,
+            rfid_number=tag,
+            original_key=tag_number,
+        )
+        log_event.save()
         return HttpResponse(content="Yes", status=200, reason="Access Allowed")
     else:
         return HttpResponse(content="No", status=403, reason="Access Denied")
-
 
 @login_required()
 def configure_rfid(request):
@@ -41,14 +50,18 @@ def configure_rfid(request):
 
 @login_required()
 def history(request):
+    twelve_hours_ago = timezone.now() + timedelta(hours=-12)
     context = {}
-    context['webunlocks'] =  WebUnlock.objects.all()
-    context['log_events'] = LogEvent.objects.all()
+    context['webunlocks'] = WebUnlock.objects.all()
+    context['log_events'] = LogEvent.objects.select_subclasses().filter(
+        created_on__gte=twelve_hours_ago,
+    ).order_by('-created_on')[:30]
     return render(request, "rfid/history.html", context)
 
 @login_required()
 def unlock(request, resource_name):
     context = {}
     resource = Resource.objects.get(name=resource_name)
-    resource.webunlock.unlock()
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
+    resource.webunlock.unlock(request.user, ip_address)
     return JsonResponse(context)
