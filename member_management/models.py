47f0f1ddd1364654727f92817911f221f0cc10cf
Sheila Miguez <github@codersquid.com>
2015-07-06 20:27:04 -0500

    add manager method for pending members
    the PersonManager now provides a method that returns all of
    the Persons who need to have their IDs checked

--- member_management/models.py
+++ member_management/models.py
@@ -2,7 +2,10 @@ from math import ceil
 from django.db import models
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
-from django.db.models import Q
+from django.db.models import (
+    Count,
+    Q,
+)
 from django.template import Context, Template
 from datetime import date
 from email.mime.application import MIMEApplication
@@ -33,6 +36,11 @@ class PersonManager(models.Manager):
     def quorum(self):
         full_members = self.get_queryset().filter(membership_status='full_member').count()
         return max(int(ceil(full_members / 3)), 1)
+
+    def pending_members(self):
+        """Get a query of all members who need IDs Checked"""
+        members = self.get_queryset().filter(Q(membership_status='full_member')|Q(membership_status='starving_hacker'))
+        return members.annotate(c=Count("idcheck")).filter(c__lte=1)
 
 
 @reversion.register
