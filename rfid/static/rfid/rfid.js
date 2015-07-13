029c2c2eea6a54b4350f87d3b2edb059f559ddff
hef <hef@pbrfrat.com>
2015-07-12 23:50:49 -0500

    unlock buttons have animations

--- rfid/static/rfid/rfid.js
+++ rfid/static/rfid/rfid.js
@@ -2,13 +2,22 @@ $(document).ready(function() {
     $('.rfid-btn').on('click', function(event){
       event.preventDefault()
       var $this = $(this);
+      $('.fa', $this).attr('class','fa fa-cog fa-spin');
       $.ajax({
           url: $this.attr('href'),
-          success: function(data){
-            alert("success")
+          success: function(data) {
+            $('.fa', $this).attr('class','fa fa-unlock')
+            window.setTimeout(function(element){
+              element.attr('class', 'fa fa-lock');
+            }, 5000, $('.fa', $this));
           },
-          error: function(data){
-            alert("error");
+          error: function(data) {
+            $('.fa', $this).attr('class','fa fa-lock')
+            $('#alerts').append(
+              '<div class="alert alert-danger alert-dismissable">'+
+              '<button class="close" aria-hidden="true" data-dismiss="alert" type="button">×</button>'+
+                'Unable to unlock the ' + $this.text().toLowerCase() + '.' +
+              '</div>');
           }
       });
     });
